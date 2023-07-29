c0c69b9945ecda7349d95fd587f5713fbc8daaa2
Mohamed Ansary <85570638+ansary29@users.noreply.github.com>
2023-07-29 11:57:59 +0530

    Add files via upload

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,8 +11,8 @@ import {
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts).post(protect, admin, createProduct)
-router.route('/:id/reviews').post(protect, createProductReview)
+router.route('/').get(getProducts).post( protect ,admin, createProduct)
+router.route('/:id/reviews').post( protect ,createProductReview)
 router.get('/top', getTopProducts)
 router
   .route('/:id')
